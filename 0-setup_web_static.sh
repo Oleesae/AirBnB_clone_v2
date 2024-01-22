03f4bc502dc7d47f77b988464d47f6752327c7ec
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-22 16:19:09 +0100

    add:setup-check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,5 +18,5 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a \tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
