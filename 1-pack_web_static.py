12d19efa95789173c099f1acd276a207669c914b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-23 12:42:57 +0100

    add:pack

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
+
+    if result.failed:
+        return None
+    return path
