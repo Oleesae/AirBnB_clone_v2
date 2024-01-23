eef32e2cb124e573a04587b7185da8165a1d1fcc
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-23 13:23:14 +0100

    add:pack1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 def do_pack():
+    """compresses a directory and its content using fabric"""
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz"
             .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
