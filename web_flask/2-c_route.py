c859e9681da1f124d257e9c6b62fe797f111c64c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-04-15 14:17:11 +0100

    add:c2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     text = text.replace("_", " ")
-    return "C {text}"
+    return "C " + text
 
 
 if __name__ == "__main__":
