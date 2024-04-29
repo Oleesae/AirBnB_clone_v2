9ca7c9ab36df8d03d367524b85ce1c20b034a3b2
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-04-29 21:26:07 +0100

    fix:deploy2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.26.122.8', '54.209.216.103']
 
 
 def do_pack():
